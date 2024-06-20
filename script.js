172e004a5832881a04a96aec8f4fd6c4399bdc62
kururuto <61511431+kururuto@users.noreply.github.com>
2024-06-19 21:14:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Check if there is a score in localStorage and set it
+    if (localStorage.getItem('score')) {
+        document.getElementById('score').innerText = 'Score: ' + localStorage.getItem('score');
+    }
+});
+
+let score = parseInt(localStorage.getItem('score')) || 0;
+
+function checkAnswer(element, answer, nextPage) {
+    // Check if the answer is correct based on the current question's correct answer
+    if ((document.title === 'Q1' && answer === 'C') || 
+        (document.title === 'Q2' && answer === 'A') || 
+        (document.title === 'Q3' && answer === 'B')|| 
+        (document.title === 'Q4' && answer === 'B')|| 
+        (document.title === 'Q5' && answer === 'A')) {
+        score++;
+    }
+    localStorage.setItem('score', score);
+    document.getElementById('score').innerText = 'Score: ' + score;
+    
+    // Navigate to the next question page
+    window.location.href = nextPage;
+}
